160bf5ef3090d60940adc43224467e06a3dbf6c4
William Wright <william86370@gmail.com>
2020-02-10 13:31:37 -0500

    Initial commit

---
+++ assets/js/login-register.js
@@ -0,0 +1,77 @@
+
+
+
+function showRegisterForm(){
+    $('.loginBox').fadeOut('fast',function(){
+        $('.registerBox').fadeIn('fast');
+        $('.login-footer').fadeOut('fast',function(){
+            $('.register-footer').fadeIn('fast');
+        });
+        $('.modal-title').html('Register with');
+    });
+    $('.error').removeClass('alert alert-danger').html('');
+
+}
+function showLoginForm(){
+    $('#loginModal .registerBox').fadeOut('fast',function(){
+        $('.loginBox').fadeIn('fast');
+        $('.register-footer').fadeOut('fast',function(){
+            $('.login-footer').fadeIn('fast');
+        });
+
+        $('.modal-title').html('Login with');
+    });
+     $('.error').removeClass('alert alert-danger').html('');
+}
+
+function openLoginModal(){
+    showLoginForm();
+    setTimeout(function(){
+        $('#loginModal').modal('show');
+    }, 230);
+
+}
+function openRegisterModal(){
+    showRegisterForm();
+    setTimeout(function(){
+        $('#loginModal').modal('show');
+    }, 230);
+
+}
+
+function loginAjax(){
+    postData('http://localhost:3000/api/v1/Auth/CreateAccount', { username:"dd5u7777d",password:"f777u7f5ff",email:"dd7u75d" })
+        .then((data) => {
+            console.log(data); // JSON data parsed by `response.json()` call
+        });
+ //  Simulate error message from the server
+     //shakeModal();
+}
+// Example POST method implementation:
+async function postData(url = '', data = {}) {
+    // Default options are marked with *
+    const response = await fetch(url, {
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        mode: 'cors', // no-cors, *cors, same-origin
+        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: 'same-origin', // include, *same-origin, omit
+        headers: {
+            'Content-Type': 'application/json'
+            // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        redirect: 'follow', // manual, *follow, error
+        referrerPolicy: 'no-referrer', // no-referrer, *client
+        body: JSON.stringify(data) // body data type must match "Content-Type" header
+    });
+    return await response.json(); // parses JSON response into native JavaScript objects
+}
+
+function shakeModal(){
+    $('#loginModal .modal-dialog').addClass('shake');
+             $('.error').addClass('alert alert-danger').html("Invalid email/password combination");
+             $('input[type="password"]').val('');
+             setTimeout( function(){
+                $('#loginModal .modal-dialog').removeClass('shake');
+    }, 1000 );
+}
+
